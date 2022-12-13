bb111d51bd83f8c1e3daeb7a0b2358d9e8a7e826
Lucas <lucas75maia@gmail.com>
2022-12-13 19:35:24 -0300

    Atualização da função

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-let array = [1, 2, 5, 7, 4]
-let valorSoma = 7
-//Pegar soma 12
-
-for (i = 0; i < array.length; i++) {
-    for (c = 1; c < array.length + 1; c++) {
-        if (array[i] + array[c] == valorSoma) {
-            console.log("índices:", i, c)
-            break;
+function Somar(array, valorSoma) {
+    for (i = 0; i < array.length; i++) {
+        for (c = 1; c < array.length + 1; c++) {
+            if (array[i] + array[c] == valorSoma) {
+                return console.log(array[i], `(posição ${i})`, " + ", array[c], `(posição ${c})`, " = ", valorSoma)
+                i = array.length
+                break;
+            }
         }
     }
 }
+
+Somar([1, 2, 5, 7, 4], 7)
